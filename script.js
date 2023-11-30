63dbb54f532c92e3bbce5e7b30546ef36e591575
Yousaf Nazir <yousaf.nazir2023@gmail.com>
2023-12-01 02:53:52 +0500

    second commit

--- script.js
+++ script.js
@@ -45,11 +45,64 @@ animateProgressBar('progress-bar-2', 'percentage-2', 90, 2000);
 animateProgressBar('progress-bar-3', 'percentage-3', 80, 2000); 
 animateProgressBar('progress-bar-4', 'percentage-4', 70, 2000);  
 
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll(".filter-button");
+  const galleryItems = document.querySelectorAll(".item");
+  filterButtons.forEach(button => {
+      button.addEventListener("click", function () {
+          const filterValue = this.getAttribute("data-filter");
+
+       
+          if (filterValue === "all") {
+              galleryItems.forEach(item => {
+                  item.style.display = "block";
+              });
+          } else {
+             
+              galleryItems.forEach(item => {
+                  if (item.classList.contains(filterValue)) {
+                      item.style.display = "block";
+                  } else {
+                      item.style.display = "none";
+                  }
+              });
+          }
+
+          filterButtons.forEach(btn => {
+              btn.classList.remove("active");
+          });
+          this.classList.add("active");
+      });
+  });
+});
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
 const sr = ScrollReveal ({
   distance: '65px',
   duration: 2600,
   delay: 450,
-  reset: true
+  reset: false
 });
 sr.reveal('.content-div' ,{delay:200, origin:'top'});
 sr.reveal('.m-p' ,{delay:450, origin:'top'});
